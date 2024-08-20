cd4d3a14606762e8eff0d3c2950caa109080f098
Jay Panchal <jaypanchal4498@gmail.com>
2024-08-20 15:50:44 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/tofro/flutternativetofro/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.tofro.flutternativetofro
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+
+    var channelname = "nativetoflutter";
+
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        var chanel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelname)
+        chanel.setMethodCallHandler { call, result ->
+            if (call.method == "getcolor") {
+                result.success("0xff60e95D")
+            } else if (call.method == "sendColor") {
+                if (call.hasArgument("color")) {
+                    result.success(call.argument("color"))
+                } else {
+                    result.success("0xff60e95D")
+                }
+            }
+        }
+
+
+    }
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToFroDemo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'ToFroDemo'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var color = "0xffffffff";
+
+  var channel = MethodChannel("nativetoflutter");
+
+  changeColor() async {
+    var result = await channel.invokeMethod("getcolor");
+    setColor(result);
+  }
+ sendColor() async {
+    var result = await channel.invokeMethod("sendColor",{"color":"0xffd1d1d1"});
+    setColor(result) ;
+    print(result);
+  }
+
+
+  setColor(String clr) {
+    setState(() {
+      color = clr;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(int.parse(color)),
+
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            /*ElevatedButton(
+                onPressed: () {
+                  setColor("0xff1f212c");
+                },
+                child: const Text("Flutter")),*/
+            ElevatedButton(
+                onPressed: changeColor, child: const Text("Get Color ")),
+            ElevatedButton(
+                onPressed: sendColor, child: const Text("Send Color "))
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
